433364a77de4b8553d4175c458ffe4df334c66bb
nari19 <kazuki7793996@gmail.com>
2019-08-07 14:32:24 +0900

    game component

--- components/Game.vue
+++ components/Game.vue
@@ -1,12 +1,41 @@
 <template>
-  <div class="VueToNuxtLogo">
-    <div class="Triangle Triangle--two" />
-    <div class="Triangle Triangle--one" />
-    <div class="Triangle Triangle--three" />
-    <div class="Triangle Triangle--four" />
+  <div class="gameboard">
+    
+    <div class="row">
+      <div class="col-4">{{ games[0] }}</div>
+      <div class="col-4">{{ games[1] }}</div>
+      <div class="col-4">{{ games[2] }}</div>
+    </div>
+    <div class="row">
+      <div class="col-4">{{ games[3] }}</div>
+      <div class="col-4">{{ games[4] }}</div>
+      <div class="col-4">{{ games[5] }}</div>
+    </div>
+    <div class="row">
+      <div class="col-4">{{ games[6] }}</div>
+      <div class="col-4">{{ games[7] }}</div>
+      <div class="col-4">{{ games[8] }}</div>
+    </div>
+
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  data() {
+    return { 
+      games: ["◻️","◻️","◻️","◻️","◻️","◻️","◻️","◻️","◻️"]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.gameboard {
+  margin: 30px auto 0 auto;
+  max-width: 450px;
+  .row div {
+    height: 15vh;
+  }
+}
 
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,10 @@
 <template>
   <div class="container">
 
-    <div class="subtitle">sanmoku_narabe</div>
+    <div class="title">sanmoku_narabe</div>
+    <Game />
 
-    <div class="row">
-      <div class="col-4">{{ games[0] }}</div>
-      <div class="col-4">{{ games[1] }}</div>
-      <div class="col-4">{{ games[2] }}</div>
-    </div>
-    <div class="row">
-      <div class="col-4">{{ games[3] }}</div>
-      <div class="col-4">{{ games[4] }}</div>
-      <div class="col-4">{{ games[5] }}</div>
-    </div>
-    <div class="row">
-      <div class="col-4">{{ games[6] }}</div>
-      <div class="col-4">{{ games[7] }}</div>
-      <div class="col-4">{{ games[8] }}</div>
-    </div>
-
-
-    <b-button variant="primary" class="pull-right">Primary Button</b-button>
+    <b-button variant="primary">Primary Button</b-button>
   </div>
 </template>
 
@@ -28,15 +12,13 @@
 import Game from '~/components/Game.vue'
 
 export default {
-  data() {
-    return { 
-      games: ["◻️","◻️","◻️","◻️","◻️","◻️","◻️","◻️","◻️"]
-    }
+  components: {
+    Game
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -45,27 +27,16 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+
+  .title {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
 
-.links {
-  padding-top: 15px;
-}
 </style>
